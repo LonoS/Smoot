c527eb09ba4cbca16c52aef4e2d5d71cefea00d3
LonoS <lonosgames@gmail.com>
2018-05-30 18:00:36 +0200

    Map is now dark

--- Smoot/MapKit/MapKitView.swift
+++ Smoot/MapKit/MapKitView.swift
@@ -10,14 +10,50 @@ import UIKit
 import MapKit
 
 
-class MapView: UIViewController{
+class MapView: UIViewController, MKMapViewDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        
+        mapView.delegate = self
+        
+
+        setupTileRenderer()
+    }
+
+    func setupTileRenderer() {
+        overlay = MapOverlay()
+        overlay!.canReplaceMapContent = true
+        mapView.add(overlay!, level: .aboveLabels)
+    }
+    var overlay : MKTileOverlay?
+    
+    
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+//        if let tileOverlay = overlay as? MKTileOverlay {
+//            return MKTileOverlayRenderer(tileOverlay: tileOverlay)
+//        } else {
+//            return MKOverlayRenderer(overlay: overlay)
+//        }
+        return MKTileOverlayRenderer.init(overlay: self.overlay!)
+    }
+    
+}
+
+class MapOverlay: MKTileOverlay{
+    
+    override func url(forTilePath path: MKTileOverlayPath) -> URL {
+
+        let template = "https://b.basemaps.cartocdn.com/rastertiles/dark_all/\(path.z)/\(path.x)/\(path.y).png"
+        
+        return URL(string: template)!
     }
     
     
 }
-
 
